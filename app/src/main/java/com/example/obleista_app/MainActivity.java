1aa4835321bbc3bae90e29c0a34dc3e658316958
Ramiro <ramiparra14@gmail.com>
2024-09-11 18:16:03 -0300

    pantalla con boton que imprime hello world al presionarlo

--- app/src/main/java/com/example/obleista_app/MainActivity.java
+++ app/src/main/java/com/example/obleista_app/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.obleista_app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Referencia al botón
+        Button button = findViewById(R.id.miBoton);
+
+        // Configuración del evento click
+        button.setOnClickListener(v -> {
+            // Muestra el mensaje Toast
+            Toast.makeText(MainActivity.this, "Hello world", Toast.LENGTH_SHORT).show();
         });
     }
 }
