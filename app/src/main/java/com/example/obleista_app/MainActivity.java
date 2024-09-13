b9e995530fb0ba4dbb7a23a60989b5018575c125
Ramiro <ramiparra14@gmail.com>
2024-09-13 15:57:47 -0300

    funcionalidad de tomar fotografías y almacenarlas

--- app/src/main/java/com/example/obleista_app/MainActivity.java
+++ app/src/main/java/com/example/obleista_app/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.obleista_app;
 
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DateFormat;
@@ -19,6 +30,10 @@ import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Uri imageUri;
+    ImageView imageView;
+    private ActivityResultLauncher<Intent> takePictureLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +61,24 @@ public class MainActivity extends AppCompatActivity {
             Log.d("botonPresionado", "BOTON PRESIONADO");
         });
 
+        // Configuración del ActivityResultLauncher para la cámara
+        takePictureLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        // Mostrar la imagen en el ImageView
+                        if (imageUri != null) {
+                            Toast.makeText(this, "Imagen guardada en: " + imageUri.toString(), Toast.LENGTH_LONG).show();
+                            imageView.setImageURI(imageUri);
+                        }
+                    }
+                });
+
         buttonCamara.setOnClickListener(v -> {
-            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-            startActivity(intent);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Uri imagePath = createImage();
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imagePath);
+            takePictureLauncher.launch(intent); // Usa el ActivityResultLauncher para iniciar la cámara
         });
 
         buttonHora.setOnClickListener(v -> obtenerHoraDelDispositivo());
@@ -56,6 +86,25 @@ public class MainActivity extends AppCompatActivity {
         buttonFecha.setOnClickListener(v -> obtenerFechaDelDispositivo());
 
         buttonHoraYFecha.setOnClickListener(v -> obtenerHoraYFechaDelDispositivo());
+    }
+
+    private Uri createImage() {
+        Uri uri = null;
+        ContentResolver resolver = getContentResolver();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            uri = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+        } else {
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+        String imgName = String.valueOf(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, imgName+".jpg");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/"+"My Images/");
+        Uri finalUri = resolver.insert(uri, contentValues);
+        imageUri = finalUri;
+        return finalUri;
+
     }
 
     /** Check if this device has a camera */
